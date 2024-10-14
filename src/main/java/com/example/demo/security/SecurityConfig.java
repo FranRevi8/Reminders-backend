7b27f4cbd9f56a71852365f02308956da9c92694
FranRevi8 <fcorevidiego@gmail.com>
2024-10-14 21:24:16 +0200

    security changes, bug fixes

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -78,7 +78,6 @@ public class SecurityConfig {
         // set up authorization for different request matchers and user roles
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/api/login/**").permitAll()
-                .requestMatchers(GET, "/api/users").hasAnyAuthority("ROLE_USER")
                 .requestMatchers(POST, "/api/users").permitAll()
                 .anyRequest().authenticated());
         // add the custom authentication filter to the http security object
